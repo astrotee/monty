dea71708db063081ed4f5052cc2673f203c86e18
amin ahmed <aminahmed24@gmail.com>
2024-01-19 02:02:32 +0200

    1. pint

--- monty.c
+++ monty.c
@@ -1,11 +1,25 @@
 #include "monty.h"
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
 global_object gb;
 
+
+/**
+* pint - print the top of the stack
+* @stack: pointer to the top of the stack
+* @line_number: the line number of the instruction
+* Return: Nothing
+*/
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+		_exiterr(stack, "L%u: can't pint, stack empty\n", line_number);
+	printf("%d\n", (*stack)->n);
+}
 
 /**
 * push - push an int to the stack
@@ -72,12 +86,14 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t nread;
 	stack_t *stack = NULL;
-	instruction_t instructions[2];
+	instruction_t instructions[3];
 
 	instructions[0].opcode = "push";
 	instructions[0].f = &push;
 	instructions[1].opcode = "pall";
 	instructions[1].f = &pall;
+	instructions[2].opcode = "pint";
+	instructions[2].f = &pint;
 	if (argc != 2)
 		_exiterr(&stack, "USAGE: monty file\n");
 	gb.file = fopen(argv[1], "r");
@@ -94,6 +110,8 @@ int main(int argc, char **argv)
 			instructions[0].f(&stack, lineno);
 		else if (strcmp("pall", opcode) == 0)
 			instructions[1].f(&stack, lineno);
+		else if (strcmp("pint", opcode) == 0)
+			instructions[2].f(&stack, lineno);
 		else
 			_exiterr(&stack, "L%u: unknown instruction %s\n", lineno, opcode);
 	}
